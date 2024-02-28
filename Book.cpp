f7dde93913d7c8d1b7ee7ad1a57d0b67ca362b57
Olti Gjoni <oltigjoni@Oltis-MacBook-Air.local>
2024-02-28 15:11:51 -0500

    base files

---
+++ Book.cpp
@@ -0,0 +1,107 @@
+#include "Book.hpp"
+
+    // write and document all methods in this file.
+    Book::Book()
+    {
+
+    }
+
+    Book::~Book()
+    {
+
+    }
+
+    Book::Book(const Book& rhs)
+    {
+
+    }
+
+    Book& Book::operator=(const Book& rhs)
+    {
+
+    }
+
+    Book::Book(Book&& rhs)
+    {
+
+    }
+
+    Book& Book::operator=(Book&& rhs)
+    {
+
+    }
+
+    const std::string& Book::getTitle() const
+    {
+
+    }
+
+    void Book::setTitle(const std::string& title)
+    {
+
+    }
+
+    const std::string& Book::getAuthor() const
+    {
+
+    }
+
+    void Book::setAuthor(const std::string& author)
+    {
+
+    }
+
+    long long int Book::getISBN() const
+    {
+
+    }
+
+    void Book::setISBN(long long int ISBN)
+    {
+
+    }
+
+    const int* Book::getIcon() const
+    {
+
+    }
+
+    void Book::setIcon(int* icon)
+    {
+
+    }
+
+    float Book::getPrice() const
+    {
+
+    }
+
+    void Book::setPrice(float price)
+    {
+
+    }
+
+    const std::vector<std::string>& Book::getKeywords() const
+    {
+
+    }
+
+    void Book::setKeywords(const std::vector<std::string>& keywords)
+    {
+
+    }
+
+    const std::string& Book::getBlurb() const
+    {
+
+    }
+
+    void Book::setBlurb(const std::string& blurb)
+    {
+
+    }
+
+    void Book::print() const
+    {
+
+    }
---
+++ MoveAll.cpp
@@ -0,0 +1,21 @@
+#include "MoveAll.hpp"
+#include "Book.hpp"
+
+// Moves all Books in "source" with the specified keyword to "dest".
+// A Book is moved if any of its keywords match the one specified in this function.
+// Use iterators whenever appropriate.
+// Hint: you need a const iterator for a const vector, but this will be automatically handled using "auto".
+
+void moveAll (const std::string keyword, std::vector<Book> &source, std::vector<Book> &dest){
+  const auto t1_start = std::chrono::steady_clock::now();
+  int books_moved=0; // counts books moved
+  // DO NOT ALTER ABOVE HERE
+
+  // erase this comment and write your code here
+
+
+  // DO NOT ALTER BELOW HERE
+  const auto t1_end = std::chrono::steady_clock::now();
+  int t1 = std::chrono::duration <double, std::micro> (t1_end - t1_start).count();
+  std::cout << "Moved " << books_moved << " books in " << t1 << " microseconds." << std::endl;
+}
